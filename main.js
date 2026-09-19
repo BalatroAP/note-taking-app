135794966bf23c25566c36b704ebf115341f0a42
BalatroAP <donovanxabad@gmail.com>
2026-09-19 15:46:49 -0700

    got basic functionality of the api working

---
+++ main.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+import "dotenv/config";
+import app from "./server.js";
+import NoteDao from "./dao/noteDAO.js";
+
+async function main() {
+  const PORT = process.env.PORT || 5000;
+
+  try {
+    const client = await mongoose.createConnection(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_COLLECTION,
+    });
+
+    NoteDao.injectDB(client);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+main();
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+import note from "./api/note.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.use("/api/v1/note", note);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "page not found" });
+});
+
+export default app;
